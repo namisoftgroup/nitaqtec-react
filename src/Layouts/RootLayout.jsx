d30f214646747780bfe66c4bba92d05156b1a722
amlbuamer2001 <devaml2001@gmail.com>
2026-01-06 11:26:03 +0200

    Reduce size of floating button to improve UI consistency

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -83,11 +83,56 @@ export default function RootLayout() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src="/images/whatsapp.svg" alt="support" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            width="512"
+            height="512"
+            x="0"
+            y="0"
+            viewBox="0 0 176 176"
+            xmlSpace="preserve"
+          >
+            <g>
+              <g data-name="Layer 2">
+                <g data-name="09.whatsapp">
+                  <circle
+                    cx="88"
+                    cy="88"
+                    r="88"
+                    fill="#29A71A"
+                    opacity="1"
+                    data-original="#29a71a"
+                  ></circle>
+                  <g fill="#FFFFFF">
+                    <path
+                      d="M126.8 49.2a54.57 54.57 0 0 0-87.42 63.13l-5.79 28.11a2.08 2.08 0 0 0 .33 1.63 2.11 2.11 0 0 0 2.24.87l27.55-6.53A54.56 54.56 0 0 0 126.8 49.2zm-8.59 68.56a42.74 42.74 0 0 1-49.22 8l-3.84-1.9-16.89 4 .05-.21 3.5-17-1.88-3.71a42.72 42.72 0 0 1 7.86-49.59 42.73 42.73 0 0 1 60.42 0 2.28 2.28 0 0 0 .22.22 42.72 42.72 0 0 1-.22 60.19z"
+                      fill="#FFFFFF"
+                      opacity="1"
+                      data-original="#ffffff"
+                    ></path>
+                    <path
+                      d="M116.71 105.29c-2.07 3.26-5.34 7.25-9.45 8.24-7.2 1.74-18.25.06-32-12.76l-.17-.15C63 89.41 59.86 80.08 60.62 72.68c.42-4.2 3.92-8 6.87-10.48a3.93 3.93 0 0 1 6.15 1.41l4.45 10a3.91 3.91 0 0 1-.49 4l-2.25 2.92a3.87 3.87 0 0 0-.35 4.32c1.26 2.21 4.28 5.46 7.63 8.47 3.76 3.4 7.93 6.51 10.57 7.57a3.82 3.82 0 0 0 4.19-.88l2.61-2.63a4 4 0 0 1 3.9-1l10.57 3a4 4 0 0 1 2.24 5.91z"
+                      fill="#FFFFFF"
+                      opacity="1"
+                      data-original="#ffffff"
+                    ></path>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
         </a>
 
         <a href="tel:+966539333104" className="floating_btn">
-          <img src="/images/phone.svg" alt="support" />
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" x="0" y="0" viewBox="0 0 512 512" xml:space="preserve">
+            <g>
+                <circle cx="256" cy="256" r="256"  fill="#2196F3" data-original="#2196f3">
+                </circle>
+                <path d="M384 308.928c-27.616 0-53.952-6.016-78.24-17.888a16.267 16.267 0 0 0-12.256-.736 16.138 16.138 0 0 0-9.184 8.16l-11.52 23.84c-34.56-19.84-63.232-48.544-83.104-83.104l23.872-11.52c3.84-1.856 6.752-5.152 8.16-9.184 1.376-4.032 1.12-8.448-.736-12.256-11.904-24.256-17.92-50.592-17.92-78.24 0-8.832-7.168-16-16-16H128c-8.832 0-16 7.168-16 16 0 149.984 122.016 272 272 272 8.832 0 16-7.168 16-16v-59.072c0-8.832-7.168-16-16-16z" fill="#FAFAFA" data-original="#fafafa"></path>
+            </g>
+        </svg>
         </a>
       </div>
       {/* <RequestServiceModal show={showModal} setShow={setShowModal} /> */}
